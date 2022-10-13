bac3885fd5ea4cd6e5142b11ba33a6bf45525fa9
SztGellert <sztankgellert@gmail.com>
2022-10-13 22:34:31 +0200

    Added custom block class and instances

--- src/main/java/net/sztgellert/gellertmod/GellertMod.java
+++ src/main/java/net/sztgellert/gellertmod/GellertMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.sztgellert.gellertmod.block.ModBlocks;
 import net.sztgellert.gellertmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -21,6 +22,7 @@ public class GellertMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
